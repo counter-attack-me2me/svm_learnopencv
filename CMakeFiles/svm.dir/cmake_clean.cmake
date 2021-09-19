8a8a48fe6d1017a14bbcb0631a17659428a3b4b1
counter-attack-me2me <1228505256@qq.com>
2021-09-19 09:26:19 +0000

    add results file

---
+++ CMakeFiles/svm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/svm.dir/src/train_digits.cpp.o"
+  "svm"
+  "svm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/svm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
